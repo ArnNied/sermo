a19281ef60cfeb49a85530b518e0b4c4ea4afa93
Andrien Wiandyano <arnnied03@gmail.com>
2023-08-21 15:33:44 +0700

    fix: remove duplicate middleware being used

--- functions/src/core.ts
+++ functions/src/core.ts
@@ -1,25 +1,22 @@
-import cors from "cors";
-import express from "express";
-import * as admin from "firebase-admin";
-import Pusher from "pusher";
-
-import {deleteStaleUser} from "./middleware/user";
+import cors from 'cors';
+import express from 'express';
+import * as admin from 'firebase-admin';
+import Pusher from 'pusher';
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin: true}));
-app.use(deleteStaleUser);
+app.use(cors({ origin: true }));
 
 // The Firebase Admin SDK to access Firestore.
 admin.initializeApp();
 
 const PUSHER = new Pusher({
-  appId: process.env.PUSHER_APP_ID || "your_pusher_app_id",
-  key: process.env.PUSHER_KEY || "your_pusher_key",
-  secret: process.env.PUSHER_SECRET || "your_pusher_secret",
-  cluster: process.env.PUSHER_CLUSTER || "your_pusher_cluster",
-  useTLS: true,
+  appId: process.env.PUSHER_APP_ID || 'your_pusher_app_id',
+  key: process.env.PUSHER_KEY || 'your_pusher_key',
+  secret: process.env.PUSHER_SECRET || 'your_pusher_secret',
+  cluster: process.env.PUSHER_CLUSTER || 'your_pusher_cluster',
+  useTLS: true
 });
 
-export {admin, PUSHER, app};
+export { admin, PUSHER, app };
